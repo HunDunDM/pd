87dc2f02666bafd94c9b05702b75438fd6ebbfc4
HunDunDM <hundundm@gmail.com>
2025-11-26 12:37:07 +0800

    tiny fix
    Signed-off-by: HunDunDM <hundundm@gmail.com>

--- pkg/schedule/affinity/policy.go
+++ pkg/schedule/affinity/policy.go
@@ -40,7 +40,7 @@ var (
 func (m *Manager) ObserveAvailableRegion(region *core.RegionInfo, group *GroupState) {
 	// Use the peer distribution of the first observed available Region as the result.
 	// TODO: Improve the strategy.
-	if group == nil || group.IsAffinitySchedulingAllowed || !m.IsAvailable() {
+	if group == nil || !group.IsBalanceSchedulingAllowed {
 		return
 	}
 	leaderStoreID := region.GetLeader().GetStoreId()
